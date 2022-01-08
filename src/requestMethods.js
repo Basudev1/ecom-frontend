110a364feb53cd3ecbb0fc7995c92a2ae3bc2156
Basudev <murabasudev@gmail.com>
2022-01-08 10:19:08 +0530

    Captured the access token from local storage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const BASE_URL = "http://localhost:3000/api/v1/";
-const Token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MWQ1OTdjOWNmNzViNmM0MjA4Yjc3MTEiLCJ1c2VybmFtZSI6ImFkbWluIiwiaXNBZG1pbiI6ImZhbHNlIiwiaWF0IjoxNjQxNDY0NTE3LCJleHAiOjE2NDQwNTY1MTd9.SzbDiCKBr_JL-3mlr6R6KzW0I7OEilpqhmc4ZkRs5S8";
+const Token = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+  .currentUser.token;
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
