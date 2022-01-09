008d6d068a1d05867e87ae9b3f3935ce219cf7b8
Basudev <murabasudev@gmail.com>
2022-01-09 15:06:57 +0530

    Minor Bug Fix

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -4,13 +4,20 @@ const BASE_URL = "http://localhost:3000/api/v1/";
 //   JSON.parse(localStorage.getItem("persist:root")).user
 // ).currentUser.token;
 
+// console.log(
+//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+//     .currentUser == null
+// );
+
 const Token =
   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-    .currentUser === null
+    .currentUser == null
     ? ""
     : JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
         .currentUser.token;
+
 // const Token = "";
+// console.log(Token);
 // const Token = `${getToken}?!=null`;
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
